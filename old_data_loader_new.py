f6556c83cb7c1a335eb21b99f1d212b367d770b3
smutuvi <stevemutuvi@gmail.com>
2021-01-08 09:29:18 +0300

    updated data_loader_new

--- old_data_loader_new.py
+++ old_data_loader_new.py
@@ -23,9 +23,11 @@ class InputExample(object):
         specified for train and dev examples, but not for test examples.
     """
 
-    def __init__(self, guid, text_a, label, true = -1):
+    # def __init__(self, guid, text_a, label, true = -1):
+    def __init__(self, guid, text_a, text_b=None, label=None, true = -1):
         self.guid = guid
         self.text_a = text_a
+        self.text_b = text_b
         self.label = label
         self.true = true
 
@@ -91,6 +93,7 @@ class MaskedLmInstance(object):
         """Serializes this instance to a JSON string."""
         return json.dumps(self.to_dict(), indent=2, sort_keys=True) + "\n"
 
+
 class InputFeatures(object):
     """
     A single set of features of data.
@@ -102,8 +105,8 @@ class InputFeatures(object):
         token_type_ids: Segment token indices to indicate first and second portions of the inputs.
     """
 
-    def __init__(self, input_ids, attention_mask, token_type_ids, label_id, true = -1,
-                 e1_mask = None, e2_mask = None, keys=None):
+    def __init__(self, input_ids, attention_mask, input_mask, token_type_ids, label_id, true = -1,
+                 e1_mask = None, e2_mask = None, keys=None, segment_ids=None, valid_ids=None, label_mask=None):
         self.input_ids = input_ids
         self.attention_mask = attention_mask
         self.token_type_ids = token_type_ids
@@ -112,6 +115,12 @@ class InputFeatures(object):
         self.e1_mask = e1_mask
         self.e2_mask = e2_mask
         self.keys=keys
+
+        # added
+        self.input_mask = input_mask
+        self.segment_ids = segment_ids
+        self.valid_ids = valid_ids
+        self.label_mask = label_mask
 
     def __repr__(self):
         return str(self.to_json_string())
@@ -125,42 +134,88 @@ class InputFeatures(object):
         """Serializes this instance to a JSON string."""
         return json.dumps(self.to_dict(), indent=2, sort_keys=True) + "\n"
 
+def readfile(fpath):
+    f = open(fpath)
+    data = []
+    sentence = []
+    label= []
+    for line in f:
+        if len(line)==0 or line.startswith('-DOCSTART') or line[0]=="\n":
+            if len(sentence) > 0:
+                data.append((sentence,label))
+                sentence = []
+                label = []
+            continue
+        splits = line.split(' ')    
+        # print(splits[0])
+        sentence.append(splits[0])
+        label.append(splits[-1][:-1])
 
-class SemEvalProcessor(object):
-    """Processor for the Semeval data set """
+    if len(sentence) >0:
+        data.append((sentence,label))
+        sentence = []
+        label = []
+    # print(data)
+    return data
+    
+class DataProcessor(object):
+    """Base class for data converters for sequence classification data sets."""
 
-    def __init__(self, args):
-        self.args = args
-        self.relation_labels = get_label(args)
-        self.num_label = 3
-        # self.relation_labels = [x for x in range(self.num_label)]
-        self.label2id = {x:x for x in range(self.num_label)}
-        self.id2label = {x:x for x in range(self.num_label)}
+    def get_train_examples(self, data_dir):
+        """Gets a collection of `InputExample`s for the train set."""
+        raise NotImplementedError()
+
+    def get_dev_examples(self, data_dir):
+        """Gets a collection of `InputExample`s for the dev set."""
+        raise NotImplementedError()
+
+    def get_labels(self):
+        """Gets the list of labels for this data set."""
+        raise NotImplementedError()
 
     @classmethod
     def _read_tsv(cls, input_file, quotechar=None):
         """Reads a tab separated value file."""
-        with open(input_file, "r", encoding="utf-8") as f:
-            reader = csv.reader(f, delimiter="\t", quotechar=quotechar)
-            lines = []
-            for line in reader:
-                lines.append(line)
-            return lines
+        return readfile(input_file)
 
-    def _create_examples(self, lines, set_type):
-        """Creates examples for the training and dev sets."""
+        
+class NerProcessor(DataProcessor):
+    """Processor for the CoNLL-2003 data set."""
+
+    def __init__(self, args):
+        self.args = args
+
+    # def get_train_examples(self, data_dir):
+    #     """See base class."""
+    #     return self._create_examples(
+    #         self._read_tsv(os.path.join(data_dir, "train.txt")), "train")
+    
+    # def get_dev_examples(self, data_dir):
+    #     """See base class."""
+    #     return self._create_examples(
+    #         self._read_tsv(os.path.join(data_dir, "valid.txt")), "dev")
+    
+    # def get_test_examples(self, data_dir):
+    #     """See base class."""
+    #     return self._create_examples(
+    #         self._read_tsv(os.path.join(data_dir, "test.txt")), "test")
+
+    def get_labels(self):
+        return ["O", "B-MISC", "I-MISC",  "B-PER", "I-PER", "B-ORG", "I-ORG", "B-LOC", "I-LOC", "[CLS]", "[SEP]"]
+
+    def _create_examples(self,lines,set_type):
         examples = []
-        for (i, line) in enumerate(lines):
+        for i,(sentence,label) in enumerate(lines):
             guid = "%s-%s" % (set_type, i)
-            # # print(line[1])
-            # text_a = line[1]
-            # # print(text_a)
-            # label = self.relation_labels.index(line[0])
-            text_a = line[1]
-            label = line[0]
-            if i % 1000 == 0:
-                logger.info(line)
-            examples.append(InputExample(guid=guid, text_a=text_a, label=label))
+            text_a = ' '.join(sentence)
+            text_b = None
+            if set_type in ['unlabeled']:
+                for x in range(len(sentence)):
+                    label[x] = -1
+                # label = -1
+            else:
+                label = label
+            examples.append(InputExample(guid=guid,text_a=text_a,text_b=text_b,label=label))
         return examples
 
     def get_examples(self, mode):
@@ -178,9 +233,58 @@ class SemEvalProcessor(object):
         elif mode == 'unlabeled':
             file_to_read = self.args.unlabel_file
 
-        print(self.args.data_dir)
-        print(file_to_read)
-        
+        logger.info("LOOKING AT {}".format(os.path.join(self.args.data_dir, file_to_read)))
+        return self._create_examples(self._read_tsv(os.path.join(self.args.data_dir, file_to_read)), mode)
+
+
+class SemEvalProcessor(object):
+    """Processor for the Semeval data set """
+
+    def __init__(self, args):
+        self.args = args
+        self.relation_labels = get_label(args)
+        self.num_label = 3
+        self.label2id = None
+        self.id2label = None
+
+
+    @classmethod
+    def _read_tsv(cls, input_file, quotechar=None):
+        """Reads a tab separated value file."""
+        with open(input_file, "r", encoding="utf-8") as f:
+            reader = csv.reader(f, delimiter="\t", quotechar=quotechar)
+            lines = []
+            for line in reader:
+                lines.append(line)
+            return lines
+
+    def _create_examples(self, lines, set_type):
+        """Creates examples for the training and dev sets."""
+        examples = []
+        for (i, line) in enumerate(lines):
+            guid = "%s-%s" % (set_type, i)
+            # print(line[1])
+            text_a = line[1]
+            # print(text_a)
+            label = self.relation_labels.index(line[0])
+            if i % 1000 == 0:
+                logger.info(line)
+            examples.append(InputExample(guid=guid, text_a=text_a, label=label))
+        return examples
+
+    def get_examples(self, mode):
+        """
+        Args:
+            mode: train, dev, test
+        """
+        file_to_read = None
+        if mode == 'train':
+            file_to_read = self.args.train_file
+        elif mode == 'dev':
+            file_to_read = self.args.dev_file
+        elif mode == 'test':
+            file_to_read = self.args.test_file
+
         logger.info("LOOKING AT {}".format(os.path.join(self.args.data_dir, file_to_read)))
         return self._create_examples(self._read_tsv(os.path.join(self.args.data_dir, file_to_read)), mode)
 
@@ -466,6 +570,7 @@ class ChemprotProcessor(object):
 
 processors = {
     "semeval": SemEvalProcessor,
+    "epidemic": NerProcessor,
     "yelp2": YelpProcessor,
     "youtube": YoutubeProcessor,
     "imdb": YoutubeProcessor,
@@ -803,6 +908,93 @@ def convert_examples_to_features(examples, max_seq_len, tokenizer,
 
     return features
 
+def convert_examples_to_features_ner(examples, label_list, max_seq_length, tokenizer):
+    """Loads a data file into a list of `InputBatch`s."""
+
+    label_map = {label : i for i, label in enumerate(label_list,1)}
+    
+    features = []
+    for (ex_index,example) in enumerate(examples):
+        textlist = example.text_a.split(' ')
+        labellist = example.label
+        tokens = []
+        labels = []
+        valid = []
+        label_mask = []
+        for i, word in enumerate(textlist):
+            token = tokenizer.tokenize(word)
+            tokens.extend(token)
+            label_1 = labellist[i]
+            for m in range(len(token)):
+                if m == 0:
+                    labels.append(label_1)
+                    valid.append(1)
+                    label_mask.append(1)
+                else:
+                    valid.append(0)
+        if len(tokens) >= max_seq_length - 1:
+            tokens = tokens[0:(max_seq_length - 2)]
+            labels = labels[0:(max_seq_length - 2)]
+            valid = valid[0:(max_seq_length - 2)]
+            label_mask = label_mask[0:(max_seq_length - 2)]
+        ntokens = []
+        segment_ids = []
+        label_ids = []
+        ntokens.append("[CLS]")
+        segment_ids.append(0)
+        valid.insert(0,1)
+        label_mask.insert(0,1)
+        label_ids.append(label_map["[CLS]"])
+        for i, token in enumerate(tokens):
+            ntokens.append(token)
+            segment_ids.append(0)
+            if len(labels) > i:
+                label_ids.append(label_map[labels[i]])
+        ntokens.append("[SEP]")
+        segment_ids.append(0)
+        valid.append(1)
+        label_mask.append(1)
+        label_ids.append(label_map["[SEP]"])
+        input_ids = tokenizer.convert_tokens_to_ids(ntokens)
+        input_mask = [1] * len(input_ids)
+        label_mask = [1] * len(label_ids)
+        while len(input_ids) < max_seq_length:
+            input_ids.append(0)
+            input_mask.append(0)
+            segment_ids.append(0)
+            label_ids.append(0)
+            valid.append(1)
+            label_mask.append(0)
+        while len(label_ids) < max_seq_length:
+            label_ids.append(0)
+            label_mask.append(0)
+        assert len(input_ids) == max_seq_length
+        assert len(input_mask) == max_seq_length
+        assert len(segment_ids) == max_seq_length
+        assert len(label_ids) == max_seq_length
+        assert len(valid) == max_seq_length
+        assert len(label_mask) == max_seq_length
+
+        if ex_index < 5:
+            logger.info("*** Example ***")
+            logger.info("guid: %s" % (example.guid))
+            logger.info("tokens: %s" % " ".join(
+                    [str(x) for x in tokens]))
+            logger.info("input_ids: %s" % " ".join([str(x) for x in input_ids]))
+            logger.info("input_mask: %s" % " ".join([str(x) for x in input_mask]))
+            logger.info(
+                    "segment_ids: %s" % " ".join([str(x) for x in segment_ids]))
+            # logger.info("label: %s (id = %d)" % (example.label, label_ids))
+
+        features.append(
+                InputFeatures(input_ids=input_ids,
+                              input_mask=input_mask,
+                              segment_ids=segment_ids,
+                              label_id=label_ids,
+                              valid_ids=valid,
+                              label_mask=label_mask))
+    return features
+
 
 def load_and_cache_examples(args, tokenizer, mode):
     if 'imdb' in args.task:
@@ -841,6 +1033,8 @@ def load_and_cache_examples(args, tokenizer, mode):
             features, = convert_examples_to_features_wic(examples, args.max_seq_len, tokenizer, add_sep_token=args.add_sep_token, task = args.task_type)
         elif args.task_type == 're':
             features = convert_examples_to_features_re(examples, args.max_seq_len, tokenizer, add_sep_token=args.add_sep_token, task = args.task_type)            
+        elif args.task_type == 'ner':
+                    features = convert_examples_to_features_ner(examples, args.max_seq_len, tokenizer, add_sep_token=args.add_sep_token, task = args.task_type)            
         else:
             features = convert_examples_to_features(examples, args.max_seq_len, tokenizer, add_sep_token=args.add_sep_token, task = args.task_type)
         logger.info("Saving features into cached file %s", cached_features_file)
@@ -856,6 +1050,12 @@ def load_and_cache_examples(args, tokenizer, mode):
     all_ids = torch.tensor([ _ for _,f in enumerate(features)], dtype=torch.long)
     size = len(features)
 
+    ##added 
+    # Convert to Tensors and build dataset
+    all_input_mask = torch.tensor([f.input_mask for f in features], dtype=torch.long)
+    all_segment_ids = torch.tensor([f.segment_ids for f in features], dtype=torch.long)
+    all_valid_ids = torch.tensor([f.valid_ids for f in features], dtype=torch.long)
+    all_lmask_ids = torch.tensor([f.label_mask for f in features], dtype=torch.long)
 
     if args.task_type == 're':
         all_e1_mask = torch.tensor([f.e1_mask for f in features], dtype=torch.long)  # add e1 mask
@@ -866,6 +1066,9 @@ def load_and_cache_examples(args, tokenizer, mode):
         all_keys = torch.tensor([f.keys for f in features], dtype=torch.long)
         dataset = TensorDataset(all_input_ids, all_attention_mask,
                                 all_token_type_ids, all_label_ids, all_ids, all_true_ids, all_keys)
+    elif args.task_type == 'ner':
+        dataset = TensorDataset(all_input_ids, all_input_mask, all_segment_ids, all_label_ids,all_valid_ids,all_lmask_ids)
+
     else:
         dataset = TensorDataset(all_input_ids, all_attention_mask,
                                 all_token_type_ids, all_label_ids, all_ids, all_true_ids)
