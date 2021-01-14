5e1e1f7a21c024cccaa8d9c29d087fb9309bdb35
steve mutuvi <stevemutuvi@gmail.com>
2021-01-14 14:15:18 +0300

    added data

--- run_ner.sh
+++ run_ner.sh
@@ -13,13 +13,16 @@ python3 run_ner.py \
   --labels=./data/labels.txt \
   --model_name_or_path=${MODEL} \
   --output_dir=${OUTPUT_DIR} \
-  --max_seq_length=${MAX_LENGTH} \
+  --max_seq_len=${MAX_LENGTH} \
   --num_train_epochs=${NUM_EPOCHS} \
   --per_gpu_train_batch_size=${BATCH_SIZE} \
   --save_steps=${SAVE_STEPS} \
   --logging_steps=${LOGGING_STEPS} \
   --seed=${SEED} \
   --task=${task} \
+	--train_file=train.txt \
+	--dev_file=dev.txt \
+	--test_file=test.txt \
   --do_train \
   --do_eval \
   --do_predict \
