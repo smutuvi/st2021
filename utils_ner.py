fc026d8fb0b68b993b1aaaf985cde12a5c12c59e
steve mutuvi <stevemutuvi@gmail.com>
2021-01-16 09:46:17 +0300

    added data

--- utils_ner.py
+++ utils_ner.py
@@ -126,14 +126,14 @@ class NerProcessor(DataProcessor):
             guid = "%s-%s" % (set_type, i)
             # text_a = ' '.join(sentence)
             text_a = sentence
-            label = label_
-            # if set_type in ['unlabeled']:
-            #     label = label_
-            # #     for x in range(len(sentence)):
-            # #         label[x] = -1
-            # #     # label = -1
-            # else:
-            #     label = label_
+            # label = label_
+            if set_type in ['unlabeled']:
+                label = -1
+            #     for x in range(len(sentence)):
+            #         label[x] = -1
+            #     # label = -1
+            else:
+                label = label_
             examples.append(InputExample(guid=guid, text_a=text_a, label=label))
         return examples
 
